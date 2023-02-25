8fb1d544862bf6ee43faf9f60607ca5ce2ad8f04
Orlundo Hubbard <weareprojectcode@gmail.com>
2023-02-25 10:23:30 -0600

    tweaks

---
+++ gatsby-starter-blog-master/content/blog/insurance/index.md
@@ -0,0 +1,5 @@
+---
+title: The road to a 7 figure agency
+date: "2023-02-25T22:12:03.284Z"
+description: "I'm a programmer at heart. But I've switched down the road to an insurance agency. I believe God is leading me there. The goal is to help as many people as possible."
+---
--- gatsby-starter-blog-master/gatsby-config.js
+++ gatsby-starter-blog-master/gatsby-config.js
@@ -9,10 +9,10 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `The miseducation of Orlundo Hubbard`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Orlundo Hubbard`,
+      summary: `A current Austinite sharing ideas and opinions.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
